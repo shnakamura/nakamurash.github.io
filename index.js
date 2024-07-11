796e5ae87bee9e9bec128e6f47917a9a732c257a
nakamura <60052047+shnakamura@users.noreply.github.com>
2024-07-10 22:46:45 -0300

    Refactor

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const button = document.getElementById('button-yes');
 
 button.addEventListener('mouseover', () => {
     const bounds = button.getBoundingClientRect();
+    
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
 
-    // Calculate new position within screen bounds
     const newX = Math.random() * (screenWidth - bounds.width);
     const newY = Math.random() * (screenHeight - bounds.height);
 
-    // Update button position
     button.style.position = 'absolute';
+    
     button.style.left = `${newX}px`;
     button.style.top = `${newY}px`;
 });
