d5633c8872a17c0fed7cf90c68c8efb24834931d
shnakamura <joaovtnakmura@gmail.com>
2024-07-10 22:45:59 -0300

    Site

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*{.html, .js}"],
+  theme: {
+    fontFamily: {
+      "nunito": "'Nunito', sans-serif"
+    },
+    colors: {
+      "black": "#09090a",
+      "white": "#e9e6eb",
+      "gray": "#99959c"
+    }
+  }
+}
+
